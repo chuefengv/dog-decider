ae07729f62c5978c319a382d69ed91b831a86c81
chuefengv <feng.vang98@gmail.com>
2021-03-12 21:28:29 -0500

    beginning dog choice

--- src/components/dogSize/DogSize.jsx
+++ src/components/dogSize/DogSize.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import './DogSize.css'
 
 function DogSize(){
     return(
-        <div>dogs here</div>
+        <div className='ds-wrapper'>
+            <div className='s-dog'>
+                Small Dog
+            </div>
+            <div className='m-dog'>
+                Medium Dog
+            </div>
+            <div className='l-dog'>
+                Large Dog
+            </div>
+        </div>
     );
 };
 

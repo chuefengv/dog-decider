aab16b376fe323e651ef418ee296f7acd799762b
chuefengv <feng.vang98@gmail.com>
2021-03-18 22:23:45 -0400

    hero image text finished

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,9 +4,9 @@ import './Hero.css';
 function Hero(){
     return(
         <div className='hero-wrapper'>
-            <h1>hero</h1>
-            <h1>hjksad</h1>
-            <h3>h89</h3>
+            <h1>Looking for the right dog for you?</h1>
+            <p>Dogs come in all sorts of breeds and sizes, so take the time to choose the one that matches you! </p>
+            <p>Scroll down to start your search!</p>
         </div>
     );
 };

004ea2b1242cab5b2843e8ca80fe118ce7a4333d
chuefengv <feng.vang98@gmail.com>
2021-03-20 14:54:45 -0400

    finished css, adding routes

--- src/components/header/hamnav/HamNav.jsx
+++ src/components/header/hamnav/HamNav.jsx
@@ -3,7 +3,7 @@ import './HamNav.css'
 
 function HamNav(){
     return(
-        <div>
+        <div className='hamnav-wrapper'>
             <ul className='nav-choices'>
                 <li>Home</li>
                 <li>Sizes</li>

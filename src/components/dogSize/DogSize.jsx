b6fc5be76b0173452db4dc00740827f9167ef76f
chuefengv <feng.vang98@gmail.com>
2021-03-20 12:14:26 -0400

    finished clippathing dog sizes, need to work on complementing background

--- src/components/dogSize/DogSize.jsx
+++ src/components/dogSize/DogSize.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './DogSize.css'
 import sdog_vector from '../../assets/s-dog.png'
+import mdog_vector from '../../assets/m-dog.png'
+import ldog_vector from '../../assets/l-dog.png'
+import redbone from '../../assets/background.jpeg'
+import bluebone from '../../assets/background_2.jpg'
+import greenbone from '../../assets/background_3.jpeg'
+
 
 function DogSize(){
     return(
@@ -9,15 +15,40 @@ function DogSize(){
                 <h1>Choose the right size for you!</h1>
             </div>
             <div className='small-dog'>
-                <div className='circle'>
-                    <img src={sdog_vector} alt=''></img>
+
+                <div className='dog'>
+                    <div className='container'>
+                        <div className='container-inner'>
+                            <img src={sdog_vector} alt='' className='img img1'></img>
+                            <img src={greenbone} alt='' className='circle'></img>
+                        </div>
+                    </div>
                 </div>
+
             </div>
             <div className='medium-dog'>
-                Medium Dog
+
+                <div className='dog'>
+                    <div className='container'>
+                        <div className='container-inner'>
+                            <img src={mdog_vector} alt='' className='img img1'></img>
+                            <img src={bluebone} alt='' className='circle'></img>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div className='large-dog'>
-                Large Dog
+
+                <div className='dog'>
+                        <div className='container'>
+                            <div className='container-inner'>
+                                <img src={ldog_vector} alt='' className='img img1'></img>
+                                <img src={redbone} alt='' className='circle'></img>
+                            </div>
+                        </div>
+                    </div>
+
             </div>
         </div>
     );

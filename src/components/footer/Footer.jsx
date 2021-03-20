7e8ce744c55ea80a5b21a0e67743e452f1319bb1
chuefengv <feng.vang98@gmail.com>
2021-03-20 14:18:25 -0400

    footer done, now to clean up css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import './Footer.css'
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkedin.png'
+import home from '../../assets/home.png'
+
 
 function Footer(){
     return(
@@ -9,12 +14,23 @@ function Footer(){
                      responsible dog ownership.</p>
             </div>
             <div className='social-links'>
-                <a href='/'>Github</a>
+                {/* <a href='/'>Github</a>
                 <a href='/'>LinkedIn</a>
-                <a href='/'>Personal</a>
+                <a href='/'>Personal</a> */}
+                <a href='https://github.com/chuefengv/dog-decider'>
+                    <img src={github} alt=''></img>
+                </a>
+                <a href='https://www.linkedin.com/in/chuefeng-vang-3ab565161/'>
+                    <img src={linkedin} alt=''></img>
+                </a>
+                <a href='/'>
+                    <img src={home} alt=''></img>
+                </a>
+
             </div>
             <div className='copyright'>
-                <p>copyright chuefeng vang</p>
+                <p>© Dog Data Decider, Inc. 2021. All rights reserved</p>
+                {/* <p>By Chuefeng Vang</p> */}
             </div>
         </div>
     );

d2d250355fe4a7b4c6fcc432bd9cbda4e29a255f
chuefengv <feng.vang98@gmail.com>
2021-03-19 21:33:06 -0400

    creating clipathing for sml dogs

--- src/components/dogSize/DogSize.jsx
+++ src/components/dogSize/DogSize.jsx
@@ -4,7 +4,18 @@ import './DogSize.css'
 function DogSize(){
     return(
         <div className='ds-wrapper'>
-            dogsize
+            <div className='dog-header'>
+                <h1>Choose the right size for you!</h1>
+            </div>
+            <div className='small-dog'>
+                Small Dog
+            </div>
+            <div className='medium-dog'>
+                Medium Dog
+            </div>
+            <div className='large-dog'>
+                Large Dog
+            </div>
         </div>
     );
 };

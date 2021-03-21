3ebeb5726649691ad5f44c305949c57e91773ba4
chuefengv <feng.vang98@gmail.com>
2021-03-21 13:22:31 -0400

    added express backend, time to pull data from db

---
+++ db.js
@@ -0,0 +1,13 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    password: 'password',
+    user: 'feng',
+    database: 'doginfo',
+    host: 'localhost',
+    port: '5432',
+    // ssl: {rejectUnauthorized:false}
+});
+
+module.exports = pool;
+
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const pool = require('./db.js')
+
+app.use(cors()); 
+app.use(express.json());
+
+//if deployed in production mode, serve the front end
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'))
+// }
+
+//api route to GET city query from states database
+app.get('/dogs', async(req, res)=>{
+    try{
+        const getCity = await pool.query("SELECT breed FROM dogs");
+        res.json(getCity.rows)
+    }catch(err){
+        console.log(err.message)
+    }
+})
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log(`Process is running on port ${process.env.PORT || 5000} LETS GO!`)
+
+})

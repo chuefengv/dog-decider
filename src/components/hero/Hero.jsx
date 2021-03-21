f049d0b8fbc8c8342c46833f95e3703b19e91ffa
chuefengv <feng.vang98@gmail.com>
2021-03-20 23:56:11 -0400

    fixed on hover ebugs

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Hero.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 function Hero(){
     return(
@@ -7,6 +9,12 @@ function Hero(){
             <h1>Looking for the right dog for you?</h1>
             <p>Dogs come in all sorts of breeds and sizes, so take the time to choose the one that matches you! </p>
             <p>Scroll down to start your search!</p>
+            <a href='#dogsize'>
+                <FontAwesomeIcon 
+                    className='chevron-down'
+                    icon={faChevronDown}
+                />
+            </a>
         </div>
     );
 };

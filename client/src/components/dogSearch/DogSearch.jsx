78d5f952d5680f0f1cad3bde4de119b5eda8bb13
chuefengv <feng.vang98@gmail.com>
2021-03-24 22:32:17 -0400

    adding order section to sort by size

--- client/src/components/dogSearch/DogSearch.jsx
+++ client/src/components/dogSearch/DogSearch.jsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import './DogSearch.css';
 import Axios from 'axios';
-import Button from '@material-ui/core/Button'
-import ModalWindow from './modal/Modal'
+import Button from '@material-ui/core/Button';
+import ModalWindow from './modal/Modal';
+import Ordering from './ordering/Ordering';
 import Modal from '@material-ui/core/Modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
@@ -34,6 +35,9 @@ function DogSearch(){
                 <div className='dog-search-header-title'>
                     <h1>Dog Breeds</h1>
                 </div>
+            </div>
+            <div className='dog-search-order'>
+                <Ordering />
             </div>
         
             <div className='dog-results'>
---
+++ client/src/components/dogSearch/ordering/Ordering.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Ordering.css';
+
+function Ordering(){
+    return(
+        <div>
+            Order
+        </div>
+    )
+}
+
+export default Ordering;

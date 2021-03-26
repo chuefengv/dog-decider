5eee5f646bdb3a9981b835b3ec16f0d73b751798
chuefengv <feng.vang98@gmail.com>
2021-03-26 00:31:07 -0400

    added heroku pg env setup

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    password: 'password',
-    user: 'feng',
-    database: 'doginfo',
-    host: 'localhost',
+    password: (process.env.DB_PASS),
+    user: (process.env.DB_USER),
+    database: (process.env.DB),
+    host: (process.env.DB_HOST),
     port: '5432',
-    // ssl: {rejectUnauthorized:false}
+    ssl: {rejectUnauthorized:false}
 });
 
 module.exports = pool;

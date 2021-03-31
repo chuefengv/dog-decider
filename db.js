a16b26f0daab96525012be73e09d5f7f5012da4e
chuefengv <feng.vang98@gmail.com>
2021-03-31 15:04:32 -0400

    testing lcoal db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    password: (process.env.DB_PASS),
-    user: (process.env.DB_USER),
-    database: (process.env.DB),
-    host: (process.env.DB_HOST),
+    password: 'password',
+    user:  'feng',
+    database: 'doginfo',
+    host: 'localhost',
     port: '5432',
     ssl: {rejectUnauthorized:false}
 });
